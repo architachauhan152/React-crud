45dfeedea77ce552ff65f3e0fb8c717ea612f948
Ritu Chauhan <93373957+architachauhan152@users.noreply.github.com>
2023-02-17 13:05:17 +0530

    first commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound=() =>{
+    return(
+        <div>
+                <h1 className="display-1">404 Error</h1>
+        </div>
+    )
+}
+export default NotFound;
